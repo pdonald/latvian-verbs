b3261c89174e64e6c18ef3f8b51f6c3d817bb95d
Pēteris Ņikiforovs <p@mak.id.lv>
2014-01-19 11:26:28 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var http = require('http');
+var url = require('url');
+
+var verbs = [];
+
+['core', 'valerijs'].forEach(function(file) {
+    $ = cheerio.load(fs.readFileSync('morphology/dist/Lexicon_' + file + '.xml')); // load xml file
+    var array = $('*[Pamatforma$="t"], *[Pamatforma$="ties"]').map(function(index, e) { return e.attribs.pamatforma; }).toArray(); // infinitives ending in -t or -ties
+    array = array.filter(function(value, index, self) { return self.indexOf(value) === index; }); // unique
+    verbs = verbs.concat(array); // add to the list
+});
+
+console.log('Loaded ' + verbs.length + ' verbs');
+
+http.createServer(function(req, res) {
+    var query = url.parse(req.url, true).query;
+
+    var verb = query.verb;
+    var body = '<form action="?" method="get"><input type="text" name="verb" value="' + (verb || '') + '"><input type="submit"></form>';
+
+    if (verb) {
+        var matches = verbs.filter(function(candidate) {
+            return candidate.substr(2) == verb ||
+                   candidate.substr(3) == verb;
+        });
+
+        body += matches.length ? '<ul><li>' + matches.join('</li><li>') + '</li></ul>' : '<p>Nothing found</p>';
+    }
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(body);
+}).listen(8080);
